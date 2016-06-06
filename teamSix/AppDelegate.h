ee53ea3977494e18dd764d73e161baf2082429e3
ShiuhYaw <psy5851@gmail.com>
2016-06-06 17:49:45 +0800

    Initial Commit

---
+++ teamSix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  teamSix
+//
+//  Created by Shiuh Yaw Phang on 06/06/2016.
+//  Copyright © 2016 Shiuh Yaw Phang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ teamSix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teamSix
+//
+//  Created by Shiuh Yaw Phang on 06/06/2016.
+//  Copyright © 2016 Shiuh Yaw Phang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
